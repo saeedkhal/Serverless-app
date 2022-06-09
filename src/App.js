37354a31480e210f936b0c329850a74ad83782b1
saeedkhal <nnowyan@gmail.com>
2022-06-09 15:43:33 +0200

    start use netlify serverless functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchData = async () => {
+    // const { data } = await axios.get('/.netlify/functions/hello');
+    const { data } = await axios.get('/api/hello');
+    console.log(data);
+  };
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, []);
+  return <div className="App">Hello</div>;
 }
 
 export default App;
